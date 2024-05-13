39ab4fb2eb67d43e456a0b456c4fe8664eb5c943
Satomi Nea <thexamlomaximal@gmail.com>
2024-05-13 23:45:32 +0300

    Update cube example model as OBJ resource

--- game/src/main/java/game/graphics/CubeExample.java
+++ game/src/main/java/game/graphics/CubeExample.java
@@ -1,15 +1,9 @@
 package game.graphics;
 
-import org.joml.Vector2f;
-import org.joml.Vector3f;
-
 import com.xamlo.core.engine.graphics.api.gui.AbstractSceneElement;
 import com.xamlo.core.engine.graphics.components.GraphicalMesh;
-import com.xamlo.core.engine.graphics.components.attribs.PositionAttribute;
-import com.xamlo.core.engine.graphics.components.attribs.TexCoordAttribute;
-import com.xamlo.core.engine.graphics.primitives.Vertex;
-import com.xamlo.core.engine.graphics.primitives.VertexStructure;
-import com.xamlo.engine.api.resources.IVertex;
+import com.xamlo.engine.api.resources.IModelResource;
+import com.xamlo.engine.resources.ResourceLoader;
 
 public class CubeExample extends AbstractSceneElement {
 
@@ -18,7 +12,8 @@ public class CubeExample extends AbstractSceneElement {
 	 */
 	protected static GraphicalMesh mesh;
 
-	
+	protected static IModelResource<String> model;
+
 	public CubeExample() {
 		super();
 	}
@@ -26,74 +21,8 @@ public class CubeExample extends AbstractSceneElement {
 	@Override
 	public void loadMesh() {
 
-    	IVertex[] vertices = new Vertex[8];
-    	int i = 0;
-    	
-    	VertexStructure vertexScruct = new VertexStructure();
-    	vertexScruct.addAttribute(new PositionAttribute());
-    	vertexScruct.addAttribute(new TexCoordAttribute());
-    	vertexScruct.setVertexCount(8);
-    	
-    	vertices[i++] = new Vertex(5).append(new Vector3f(-0.5f,  0.5f, 1.0f)).append(new Vector2f(0.0f, 0.0f)); //V1
-    	vertices[i++] = new Vertex(5).append(new Vector3f(-0.5f, -0.5f, 1.0f)).append(new Vector2f(0.0f, 1.0f)); //V2
-    	vertices[i++] = new Vertex(5).append(new Vector3f( 0.5f, -0.5f, 1.0f)).append(new Vector2f(1.0f, 1.0f)); //V3
-    	vertices[i++] = new Vertex(5).append(new Vector3f( 0.5f,  0.5f, 1.0f)).append(new Vector2f(1.0f, 0.0f)); //V4
-    	vertices[i++] = new Vertex(5).append(new Vector3f(-0.5f,  0.5f, -1.0f)).append(new Vector2f(0.0f, 0.0f)); //V1-1
-    	vertices[i++] = new Vertex(5).append(new Vector3f(-0.5f, -0.5f, -1.0f)).append(new Vector2f(0.0f, 1.0f)); //V2-1
-    	vertices[i++] = new Vertex(5).append(new Vector3f( 0.5f, -0.5f, -1.0f)).append(new Vector2f(1.0f, 1.0f)); //V3-1
-    	vertices[i++] = new Vertex(5).append(new Vector3f( 0.5f,  0.5f, -1.0f)).append(new Vector2f(1.0f, 0.0f)); //V4-1
-
-    	i = 0;
-    	int[] indices = new int[36]; 
-    	//FACE
-    	indices[i++] = 0;
-    	indices[i++] = 1;
-    	indices[i++] = 3;
-    	
-    	indices[i++] = 3;
-    	indices[i++] = 1;
-    	indices[i++] = 2;
-    	//LEFT
-    	indices[i++] = 0;
-    	indices[i++] = 1;
-    	indices[i++] = 5;
-    	
-    	indices[i++] = 0;
-    	indices[i++] = 4;
-    	indices[i++] = 5;
-    	//RIGHT
-    	indices[i++] = 3;
-    	indices[i++] = 2;
-    	indices[i++] = 7;
-    	
-    	indices[i++] = 2;
-    	indices[i++] = 6;
-    	indices[i++] = 7;
-    	//BEHIND
-    	indices[i++] = 4;
-    	indices[i++] = 5;
-    	indices[i++] = 7;
-    	
-    	indices[i++] = 7;
-    	indices[i++] = 5;
-    	indices[i++] = 6;
-    	///TOP
-    	indices[i++] = 0;
-    	indices[i++] = 3;
-    	indices[i++] = 4;
-    	
-    	indices[i++] = 3;
-    	indices[i++] = 7;
-    	indices[i++] = 4;
-    	///BUTTOM
-    	indices[i++] = 1;
-    	indices[i++] = 5;
-    	indices[i++] = 6;
-    	
-    	indices[i++] = 1;
-    	indices[i++] = 2;
-    	indices[i++] = 6;
-        mesh = new GraphicalMesh(vertices, vertexScruct, indices);
+		model = ResourceLoader.INSTANCE().loadModel("model.example.cube");
+        mesh = new GraphicalMesh(model.getVertices(), model.getStructure(), model.getIndices());
         
 	}
 	
